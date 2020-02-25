d5ca59356b506ea1697efd2ec38cd91c620a0658
Elena Lucas <elenalucasserrano@Elenas-MacBook-Pro.local>
2020-02-25 20:57:35 +0100

    model done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
-const data = require('./data.js'); // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js' 
+
+//ITERATION3
+//We are importing an array of recipes form the data.js file. Using the Model.insertMany method, you should add that entire array to the database. After inserting the elements, print on the console the title of each recipe.
 
 // Connection to the database "recipeApp"
 mongoose
@@ -11,3 +14,17 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+//ITEARATION2
+//   In index.js, using the Model.create method, you should pass the info to create a new recipe. After the creation, you can use MongoDB Compass to check everything went ok. After inserting the recipe, console.log the title of the recipe.
+// To run your code, remember you should use:
+// $ node index.js
+
+//Iteration 4 - Update recipe
+//Now you should have six different recipes in the database, but there was a mistake in one of them. The Rigatoni alla Genovese does not take that long. You should update the duration field and set it to 100. After updating it, print a success message!
+
+//Iteration 5 - Remove a recipe
+//Oh oh! The Carrot Cake is no longer available, so we need to remove it from the database. Using the Model.deleteOne method, remove that recipe from the database and display a success message after doing it!
+
+//Iteration 6 - Close the Database
+//After doing all the task you should close the database. Otherwise, the connection will keep open. Be careful about the asynchronicity of all process; you should close it after everything is done! 😉
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,8 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: String,
+  ingredients: Array;
+  cuisine: String,
+  dishTypeimage: String,
+  image: String,
+  duration: Number,
+  creator: String,
+  ceated: Date
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+
+//ITERATION1
+// title - type String. It should be required and unique.
+// level - type String. Only can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef (remember the ENUM 😉)
+// ingredients - type Array.
+// cuisine - type String. Should be required.
+// dishType - type String. Possible values: Breakfast - Dish - Snack - Drink - Dessert - Other.
+// image - type String. Default value: https://images.media-allrecipes.com/images/75131.jpg.
+// duration - type Number. Min value should be 0.
+// creator - type String
+// created - type Date. By default today.
+
+
+//https://mongoosejs.com/docs/api.html#model-js
